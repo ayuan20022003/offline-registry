9d8f4d0fc49b36dd7c76801904f267ca5498a0a4
root <75710541@qq.com>
2016-09-10 23:00:26 +0800

    update imagelist

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -x
+set -e
 BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 registry_image_name=`awk '/centos7-docker-registry/{print $2}' docker-compose.yml`
-docker offline-images|grep $registry_image_name || docker load -i ../offline-images/registry.tar.gz
+docker images|grep $registry_image_name || docker load -i ../offline-images/registry.tar.gz
 docker-compose -p offline up -d
