c46b18727672893415a83bc4d6b6716957cdba2e
root <75710541@qq.com>
2016-11-02 17:26:11 +0800

    update load_registry_data.sh

--- load_registry_data.sh
+++ load_registry_data.sh
@@ -13,11 +13,11 @@ offline_registry="offlineregistry.dataman-inc.com:5000"
 
 mkdir -p ../offline-registry_data
 mkdir -p ../offline-images
-
+num=0
 load_offlineregistry(){
 	docker pull $online_registry/$img &>/dev/null && echo "pull $img successful." && \
 	docker tag $online_registry/$img $offline_registry/$img && \
-	docker push $offline_registry/$img &>/dev/null && echo "load $img successful." || (echo "load $img  error !!!" && exit 1)
+	docker push $offline_registry/$img &>/dev/null && echo "load $img successful." || (echo "load $img  error !!!" && num=1 && exit 1)
 }
 
 
@@ -33,3 +33,7 @@ save_registry_image(){
 }
 
 save_registry_image
+
+if [ $num -eq 0 ];then
+	echo "load all images done."
+fi
