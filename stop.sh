5edebd651399f69eb50afe40cdc9752c9fcca5ac
root <75710541@qq.com>
2017-02-09 18:30:13 +0800

    split offline-registry

--- stop.sh
+++ stop.sh
@@ -3,3 +3,7 @@ set -e
 BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 docker-compose -p offline stop 
+deadids=`docker ps -aq --filter "STATUS=dead"`
+if [ ! -z "$deadids" ];then
+        docker rm -f $deadids
+fi
