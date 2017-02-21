19f9933a4b8378bea956c1400d265c343c7e0e1f
pangzheng <pangzheng@homtail.com>
2017-02-21 13:59:27 +0800

    add offline-registry listen ip

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
+. ../offlinesry/config.cfg
+if [ x"$LOCAL_IP" == x ];then
+	exit 1
+fi
 BASE_DIR=$(cd `dirname $0` && pwd)
+CONFIG_DIR="config/registry"
 cd $BASE_DIR
 registry_image_name=`awk '/centos7-docker-registry/{print $2}' docker-compose.yml`
-docker images|grep $registry_image_name || docker load -i ../offline-images/registry.tar.gz
+if [ -f $CONFIG_DIR/config.yml ];then
+	rm -rf $CONFIG_DIR/config.yml
+fi
+cp $CONFIG_DIR/config.yml.teml $CONFIG_DIR/config.yml && \
+sed -i 's/--registry_ip--/'$LOCAL_IP'/g' $CONFIG_DIR/config.yml && \
+docker images|grep $registry_image_name || docker load -i ../offline-images/registry.tar.gz && \
 docker-compose -p offline up -d
