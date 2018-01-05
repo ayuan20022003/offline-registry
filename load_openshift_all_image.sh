efaf4ee71dafb8be861d2b6d03559dcf7fd07788
root <root@localhost.localdomain>
2018-01-05 18:10:41 +0800

    update load_openshift_all_image.sh imagelist path

--- load_openshift_all_image.sh
+++ load_openshift_all_image.sh
@@ -1,6 +1,6 @@
 BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 
-./load_concurrent_registry_data.sh ../docker.io.imagelist.txt docker.io &
-./load_concurrent_registry_data.sh ../dmos.imagelist.txt demoregistry.dataman-inc.com &
+./load_concurrent_registry_data.sh ../offline-dmos/images_manage/docker.io.imagelist.txt docker.io &
+./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.imagelist.txt demoregistry.dataman-inc.com &
 wait
