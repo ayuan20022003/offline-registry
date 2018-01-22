13f4d9a33ec837af76018bceef01df5a24c2f2fb
ss75710541 <75710541@qq.com>
2018-01-22 19:52:39 +0800

    update load_openshift_all_image.sh from devharbor

--- load_openshift_all_image.sh
+++ load_openshift_all_image.sh
@@ -2,5 +2,5 @@ BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 
 ./load_concurrent_registry_data.sh ../offline-dmos/images_manage/docker.io.imagelist.txt docker.io &
-./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.imagelist.txt demoregistry.dataman-inc.com &
+./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.imagelist.txt devharbor.dataman-inc.com &
 wait
