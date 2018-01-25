a7822497e6597cf53afe0699afab48443e553f58
ss75710541 <75710541@qq.com>
2018-01-25 14:48:23 +0800

    load registry add choose dmos.dev.imagelist.txt

--- load_openshift_all_image.sh
+++ load_openshift_all_image.sh
@@ -1,6 +1,15 @@
 BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 
+# 打包标签，dev 或 stable, dev 从 dev.imagelist.txt取镜像列表
+PACKAGEING_TAG=${1:-stable}
+
 ./load_concurrent_registry_data.sh ../offline-dmos/images_manage/docker.io.imagelist.txt docker.io &
-./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.imagelist.txt devharbor.dataman-inc.com:1443 &
+
+if [ "x$PACKAGEING_TAG" == "xdev" ];then
+	./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.dev.imagelist.txt devharbor.dataman-inc.com:1443 &
+else
+	./load_concurrent_registry_data.sh ../offline-dmos/images_manage/dmos.imagelist.txt devharbor.dataman-inc.com:1443 &
+fi
+
 wait
