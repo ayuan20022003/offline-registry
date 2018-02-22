926554e4d6ac1e47b9a62fe6a3e6a0e14071889d
ss75710541 <75710541@qq.com>
2018-02-22 11:51:55 +0800

    add delete_dmos_image.sh

---
+++ delete_dmos_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 清理offline registry 中 dmos 镜像
+BASE_DIR=$(cd `dirname $0` && pwd)
+cd $BASE_DIR
+
+../delete-docker-registry-image/delete_offline_registry_image.sh jborg/dmos-oc-ui
+../delete-docker-registry-image/delete_offline_registry_image.sh jborg/dmos-oc
+../delete-docker-registry-image/delete_offline_registry_image.sh polypite/origin-dm-web
