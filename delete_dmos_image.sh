0c76a3858b2346cf17b80166936391fba302deef
ss75710541 <75710541@qq.com>
2018-02-27 16:24:13 +0800

    update delete_dmos_image.sh

--- delete_dmos_image.sh
+++ delete_dmos_image.sh
@@ -4,5 +4,8 @@ BASE_DIR=$(cd `dirname $0` && pwd)
 cd $BASE_DIR
 
 ../delete-docker-registry-image/delete_offline_registry_image.sh jborg/dmos-oc-ui
+../delete-docker-registry-image/delete_offline_registry_image.sh jborg-dev/dmos-oc-ui
 ../delete-docker-registry-image/delete_offline_registry_image.sh jborg/dmos-oc
+../delete-docker-registry-image/delete_offline_registry_image.sh jborg-dev/dmos-oc
 ../delete-docker-registry-image/delete_offline_registry_image.sh polypite/origin-dm-web
+../delete-docker-registry-image/delete_offline_registry_image.sh polypite-dev/origin-dm-web
